ecaa63c6823737161a8ab685c3e9e7fa3031af93
BB <me@yomama.com>
2025-02-07 22:05:23 +0300

    -

--- build.py
+++ build.py
@@ -1,4 +1,4 @@
 import sys, os
 
 if sys.argv[1] == 'zip':
-    os.system("zip /opt/Downloads/dotbkps/emacs-jupyter.zip -r /home/burak/Documents/repos/emacs-jupyter/.git/")
+    os.system("zip /opt/Downloads/dotbkps/jupimacs.zip -r /home/burak/Documents/repos/jupimacs/.git/")
